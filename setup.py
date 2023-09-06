10ca933165fa558befe85a1e4a4212b9d2aed43b
Christoph Minixhofer <christoph.minixhofer@gmail.com>
2023-09-06 15:00:16 +0000

    0.1.6 -- add alignment archives

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ INSTALL_REQUIRES = [
 
 setup_kwargs = {
     'name': 'alignments',
-    'version': '0.1.5',
+    'version': '0.1.6',
     'description': '',
     'long_description': long_description,
     'license': 'MIT',
