f26ca93b86e4a026fee80bc2001d4753a1f2a7a7
Christoph Minixhofer <christoph.minixhofer@gmail.com>
2024-01-24 15:04:07 +0000

    Update libritts.py

--- alignments/datasets/libritts.py
+++ alignments/datasets/libritts.py
@@ -2,6 +2,8 @@ from glob import glob
 from pathlib import Path
 import re
 from transformers.utils.hub import cached_file
+import gzip
+import json
 
 from alignments.dataset import AlignmentDataset
 
@@ -91,3 +93,33 @@ class LibrittsRDataset(AlignmentDataset):
                 "speaker": file.parent.parent.name,
                 "transcript": transcript.upper(),
             }
+
+
+class LibriHeavyDataset(AlignmentDataset):
+    def __init__(self, **kwargs):
+        if "acoustic_model" not in kwargs:
+            kwargs["acoustic_model"] = "english_us_arpa"
+        if "g2p_model" not in kwargs:
+            kwargs["g2p_model"] = "english_us_arpa"
+        if "lexicon" not in kwargs:
+            kwargs["lexicon"] = "http://www.openslr.org/resources/11/librispeech-lexicon.txt"
+        if source_url in kwargs and kwargs["source_url"] is not None:
+            raise ValueError("source url not supported for libriheavy,\
+            please use the preprocessing at https://github.com/k2-fsa/libriheavy\
+            and specify the resulting .jsonl.gz files as \"source_directory\"")
+        if "textgrid_url" not in kwargs:
+            hf_url = "https://huggingface.co/datasets/cdminix/libritts-r-aligned/resolve/main/data/"
+            for split in "small medium large dev test_clean test_other test_clean_large test_other_large".split(" "):
+                source_split = kwargs["source_directory"].replace("libriheavy_cuts_", "")
+                source_split = source_split.replace(".jsonl.gz", "")
+                if split == source_split:
+                    kwargs["textgrid_url"] = hf_url + f"{split}.tar.gz"
+        super().__init__(**kwargs)
+
+    def collect_data(self, file):
+        with gzip.open(file, mode='r') as json_file:
+            with open(json_file, mode='r') as file:
+                for line in file:
+                    json_line = json.loads(line)
+                    print(json_line)
+                    raise
